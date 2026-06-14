6270f018e5a493a4abe6e8eeb19e21e6c8f9635c
runeanielsen <runenielsen@runbox.com>
2026-06-14 16:44:05 +0200

    remove the obsolete, we don't care in our use case

--- src/OpenFTTH.Results/Result.cs
+++ src/OpenFTTH.Results/Result.cs
@@ -91,7 +91,6 @@ namespace OpenFTTH.Results
             return result.ToResult<TValue>();
         }
 
-        [Obsolete("Removed in next major version because we could lose the Value without telling the developer. Use an explicit .ToResult() instead.")]
         public static implicit operator Result(Result<TValue> result)
         {
             return result.ToResult();
